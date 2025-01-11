2e9803848ca55bc77b9f96e7aa172eae8e45db39
copo <sebastianorellana017@gmail.com>
2025-01-11 14:46:12 -0300

    web 2

--- src/main/java/orlando/admclientes/config/CorsFilter.java
+++ src/main/java/orlando/admclientes/config/CorsFilter.java
@@ -22,9 +22,13 @@ public class CorsFilter implements Filter {
         HttpServletRequest httpServletRequest = (HttpServletRequest) request;
         HttpServletResponse httpServletResponse = (HttpServletResponse) response;
 
-        // Configura los encabezados CORS
-        httpServletResponse.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-        httpServletResponse.addHeader("Access-Control-Allow-Origin", "https://adminclientes-front.onrender.com");
+        String origin = httpServletRequest.getHeader("Origin");
+
+        // Configura dinámicamente el encabezado Access-Control-Allow-Origin
+        if (origin != null && (origin.equals("http://localhost:3000") || origin.equals("https://adminclientes-front.onrender.com"))) {
+            httpServletResponse.setHeader("Access-Control-Allow-Origin", origin);
+        }
+
         httpServletResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         httpServletResponse.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, X-Requested-With");
         httpServletResponse.setHeader("Access-Control-Allow-Credentials", "true");
